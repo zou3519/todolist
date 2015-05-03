8fe4a609c3b7c6537ac3aea1ee2242358a798f5a
zou3519 <zou3519@gmail.com>
2015-05-02 21:59:49 -0400

    mapset, also dicts fixed

--- dict.go
+++ dict.go
@@ -2,29 +2,17 @@ package main
 
 import ()
 
-// Comparable is an interface for things that are comparable to each other.
-type Comparable interface {
-	// Returns 0 if equal, > 0 if this is greater than the other
-	// < 0 if the other is greater
-	CompareTo(Comparable) int
-}
-
-// Element is to be stored inside a Set
-type Element struct {
-
-	// The value stored with this element.
-	Value *Comparable
-	// contains filtered or unexported fields
-}
-
 // Dict is an interface for data structures that can Insert, Search, and Delete
 type Dict interface {
 	// Insert element into the set
-	Insert(Element)
+	Insert(int, interface{})
 
-	// Search the set for the element.
-	Search(Element) error
+	// Search the set for the element by ID.  Return the element and T/F
+	Search(int) (interface{}, bool)
 
-	// Delete the element from the set
-	Delete(Element) error
+	// Delete the element from the set by ID.  Returns the element and T/F
+	Delete(int) (interface{}, bool)
+
+	// String gives a string representation
+	String() string
 }
---
+++ mapset.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type MapSet struct {
+	Map map[int]interface{}
+}
+
+func NewMapSet() *MapSet {
+	m := make(map[int]interface{})
+	return &MapSet{Map: m}
+}
+
+func (set *MapSet) Insert(key int, value interface{}) {
+	set.Map[key] = value
+}
+
+func (set *MapSet) Search(key int) (interface{}, bool) {
+	value, ok := set.Map[key]
+	if !ok {
+		return nil, ok
+	} else {
+		return value, true
+	}
+}
+
+func (set *MapSet) Delete(key int) (interface{}, bool) {
+	elt, ok := set.Map[key]
+	if ok {
+		delete(set.Map, key)
+		return elt, true
+	} else {
+		return nil, false
+	}
+}
+
+func (set *MapSet) String() string {
+	return fmt.Sprintln(set.Map)
+}
